5e35b1a2f884f1f58082c012d0c06d98f9876166
honee8583 <honee8583@naver.com>
2023-12-01 18:15:47 +0900

    feat: 8. 공지사항 게시판의 내용을 추상화한 모델 여러개를 반환하는 함수 작성
    - NoticeDto 모델(게시글ID, 제목, 내용, 등록일)을 반환.
    - 데이터는 (게시글 ID = 1, 제목 = 공지사항입니다, 내용 = 공지사항 내용입니다, 등록일 = 2021-01-31), (게시글 ID = 2, 제목 = 공지사항입니다, 내용 = 공지사항 내요입니다, 등록일 = 2021-02-01) 포함.
    - url : "/api/notice3".

--- src/main/java/com/example/example100/notice/controller/NoticeController.java
+++ src/main/java/com/example/example100/notice/controller/NoticeController.java
@@ -2,6 +2,8 @@ package com.example.example100.notice.controller;
 
 import com.example.example100.notice.model.NoticeDto;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,28 @@ public class NoticeController {
                 .content("공지사항 내용입니다.")
                 .regDate(LocalDateTime.of(2021, 1, 31, 17, 17))
                 .build();
+    }
+
+    /**
+     * 8. 공지사항 게시판의 내용을 추상화한 모델(게시글ID, 제목, 내용, 등록일)을 복수형태로 리턴하는 api를 작성하시오.
+     */
+    @GetMapping("/api/notice3")
+    public List<NoticeDto> getNotice3() {
+        List<NoticeDto> noticeDtos = new ArrayList<>();
+        NoticeDto noticeDto1 = NoticeDto.builder()
+                .id(1L)
+                .title("공지사항입니다.")
+                .content("공지사항 내용입니다.")
+                .regDate(LocalDateTime.of(2021, 1, 31, 17, 17))
+                .build();
+        NoticeDto noticeDto2 = NoticeDto.builder()
+                .id(2L)
+                .title("공지사항입니다.")
+                .content("공지사항 내용입니다.")
+                .regDate(LocalDateTime.of(2021, 2, 1, 17, 17))
+                .build();
+        noticeDtos.add(noticeDto1);
+        noticeDtos.add(noticeDto2);
+        return noticeDtos;
     }
 }
