3677a8aa6e69ba24546ba77cde939cd8656c4661
honee8583 <honee8583@naver.com>
2023-12-01 18:05:48 +0900

    feat: 2. "Hello World!" 문자열을 반환하는 주소를 작성
    - @RequestMapping 어노테이션을 사용하여 작성
    - 뷰페이지가 아닌 문자열을 반환하기 위해 @ResponseBody 어노테이션을 사용
    - http 파일을 이용해서 테스트

--- src/main/java/com/example/example100/hello/controller/HelloController.java
+++ src/main/java/com/example/example100/hello/controller/HelloController.java
@@ -15,4 +15,14 @@ public class HelloController {
     @RequestMapping(value = "/first-url", method= RequestMethod.GET)
     public void firstApi () {
     }
+
+    /**
+     * 2. "Hello World!" 문자열을 반환하는 주소를 작성하시오.
+     * RequestMapping 어노테이션을 사용
+     */
+    @ResponseBody
+    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    public String helloWord() {
+        return "Hello World!";
+    }
 }
