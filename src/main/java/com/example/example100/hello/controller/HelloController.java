94b620ce43073820e3746e315d27483e5aad9151
honee8583 <honee8583@naver.com>
2023-12-01 18:02:43 +0900

    feat: 1. "/first-url"을 주소로 가지는 반환값이 없는 함수를 작성
    - @RequestMapping 어노테이션을 사용하여 작성
    - http 파일을 이용해서 테스트

---
+++ src/main/java/com/example/example100/hello/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.example100.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    /**
+     * 1. "/first-url"을 주소로 가지는 반환값이 없는 주소를 작성하시오.
+     * RequestMapping 어노테이션을 사용
+     */
+    @ResponseBody
+    @RequestMapping(value = "/first-url", method= RequestMethod.GET)
+    public void firstApi () {
+    }
+}
