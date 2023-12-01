c20abec5227d7bd33ad149515f4b2ead98437705
honee8583 <honee8583@naver.com>
2023-12-01 18:00:08 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/example100/Example100Application.java
@@ -0,0 +1,13 @@
+package com.example.example100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example100Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/example100/Example100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
