ba80ba5b119f5dbb3847278f75510e909f6913a0
honee8583 <honee8583@naver.com>
2024-01-04 20:13:28 +0900

    feat: 관리자용 게시판 컨트롤러에 스웨거 문서 적용

--- src/main/java/com/example/example100/board/controller/AdminBoardController.java
+++ src/main/java/com/example/example100/board/controller/AdminBoardController.java
@@ -5,6 +5,8 @@ import com.example.example100.board.model.BoardReplyInput;
 import com.example.example100.board.service.BoardService;
 import com.example.example100.common.model.ResponseResult;
 import com.example.example100.common.model.ServiceResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Admin Board API", description = "관리자용 게시판 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 public class AdminBoardController {
@@ -24,6 +27,7 @@ public class AdminBoardController {
     /**
      * 74. 게시글의 신고 목록을 조회하는 api를 작성하시오.
      */
+    @Operation(summary = "문제74", description = "Q. 게시글의 신고 목록을 조회하는 api를 작성하시오.")
     @GetMapping("/api/admin/board/badreport")
     public ResponseEntity<?> badReportList() {
         List<BoardBadReport> boardBadReports = boardService.getBadReportList();
@@ -33,6 +37,7 @@ public class AdminBoardController {
     /**
      * 98. 문의 게시판에 답변을 달경우 메일을 전송하는 API를 작성하시오.
      */
+    @Operation(summary = "문제98", description = "Q. 문의 게시판에 답변을 달경우 메일을 전송하는 API를 작성하시오.")
     @PostMapping("/api/admin/board/{id}/reply")
     public ResponseEntity<?> reply(@PathVariable Long id, @RequestBody BoardReplyInput boardReplyInput,
                                    @RequestHeader("Authorization") String token) {
