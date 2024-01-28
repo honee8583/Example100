25911df81960c4192b8a35c89672e1db481eca06
honee8583 <honee8583@naver.com>
2024-01-28 15:41:03 +0900

    feat: 게시판 스크랩 컨트롤러에 스웨거 적용

--- src/main/java/com/example/example100/board/controller/BoardScrapController.java
+++ src/main/java/com/example/example100/board/controller/BoardScrapController.java
@@ -5,6 +5,8 @@ import com.example.example100.board.service.BoardService;
 import com.example.example100.common.model.ResponseResult;
 import com.example.example100.common.model.ServiceResult;
 import com.example.example100.util.JWTUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Board Scrap API", description = "게시판 스크랩 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 public class BoardScrapController {
@@ -21,6 +24,7 @@ public class BoardScrapController {
     /**
      * 75. 게시글의 스크랩을 추가하는 api를 작성하시오.
      */
+    @Operation(summary = "문제75", description = "Q. 게시글의 스크랩을 추가하는 api를 작성하시오.")
     @PutMapping("/api/board/{id}/scrap")
     public ResponseEntity<?> boardScrap(@PathVariable Long id,
                                         @RequestHeader("Authorization") String token) {
@@ -37,6 +41,7 @@ public class BoardScrapController {
     /**
      * 76. 게시글의 스크랩을 삭제하는 api를 작성하시오.
      */
+    @Operation(summary = "문제76", description = "Q. 게시글의 스크랩을 삭제하는 api를 작성하시오.")
     @DeleteMapping("/api/scrap/{id}")
     public ResponseEntity<?> deleteBoardScrap(@PathVariable Long id,
                                               @RequestHeader("Authorization") String token) {
